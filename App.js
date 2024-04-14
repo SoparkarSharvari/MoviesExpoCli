de98f14027152b0feca60ad53b550b48db1f1f20
SoparkarSharvari <sharvarisoparkar@gmail.com>
2024-04-14 19:15:08 +0530

    Assesment 4

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import MovieDetailScreen from './screens/MovieDetailScreen';
+import MovieScreen from './screens/MovieScreen';
 
+const Stack = createStackNavigator();
+
+// function HomeScreen(){
+//   return(
+//     <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+//     <Text>HomeScreen</Text>
+//   </View>
+//   )
+// }
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+    <Stack.Navigator initialRouteName='Movie'>
+    <Stack.Screen name="MoviesDetails" component={MovieDetailScreen}/>
+      <Stack.Screen name="Movie" component={MovieScreen}/>
+    </Stack.Navigator>
+   </NavigationContainer>
   );
 }
 
